1f447db1440a60844bc5d3952e862ece7134ac96
el-shago <92878520+el-shago@users.noreply.github.com>
2024-09-11 21:10:54 -0700

    CRUD

--- Program.cs
+++ Program.cs
@@ -1,20 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using RedesAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var dbContext = services.GetRequiredService<ApplicationDbContext>();
+        dbContext.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while applying the database migrations.");
+    }
+}
+
 app.UseSwagger();
 app.UseSwaggerUI();
-// }
 
 app.UseHttpsRedirection();
 
--- obj/Debug/net8.0/RedesAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/RedesAPI.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("2c76b545-904f-44c1-9e37-cca9c2ad5759")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("RedesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdd02a30c8f78c2e9b3e6d40b16f282042e59a7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f490cfb61b473155fc4e8a3f12063c88825531")]
 [assembly: System.Reflection.AssemblyProductAttribute("RedesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RedesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
